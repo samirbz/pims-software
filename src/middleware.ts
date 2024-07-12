d0187df1cb2c3e726de7014138ac2a6ac8ca6951
samir <febsec14@gmail.com>
2024-07-12 23:00:43 +0545

    protecting routes done

---
+++ src/middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from "next/server"
+import { auth } from "./auth"
+import { authRoutes } from "./routes"
+
+export default auth((req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return NextResponse.redirect(new URL("/members", nextUrl))
+    }
+    return NextResponse.next()
+  }
+
+  if (!isLoggedIn) {
+    return NextResponse.redirect(new URL("/", nextUrl))
+  }
+  return NextResponse.next()
+})
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|assets/images/).*)"],
+}
