cb0964fb8cec13639dd9cc84c82d36510c2a86d2
samir <febsec14@gmail.com>
2024-07-12 09:26:13 +0545

    nextui installation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
+import { Button } from "@nextui-org/react"
+import { FaRegSmile } from "react-icons/fa"
 
 export default function Home() {
   return (
-    <div>Home page</div>
-  );
+    <div>
+      <h1>Hello app</h1>
+      <Button
+        color="primary"
+        variant="bordered"
+        startContent={<FaRegSmile size={20} />}
+      >
+        Click me
+      </Button>
+    </div>
+  )
 }
