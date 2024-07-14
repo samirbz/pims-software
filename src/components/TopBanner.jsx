851ada85189dbc932040ce2faa2b797cb94b342e
samir <febsec14@gmail.com>
2024-07-14 10:19:42 +0545

    date location changed

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -6,22 +6,16 @@ import BSDateDisplay from "./BSDateDisplay"
 
 export default function TopBanner() {
   return (
-    <div className="relative flex h-[6.4rem] w-full items-center justify-between">
+    <div className="flex h-[6.4rem] w-full items-center justify-between">
       <div className="flex justify-start">
-        <Link href="/" className="mb-4">
-          <Image src="/images/logo.png" width={320} height={70} alt="PIMS" />
+        <Link href="/" className="">
+          <Image src="/images/logo.png" width={300} height={80} alt="PIMS" />
         </Link>
       </div>
-      <div className="absolute bottom-0 left-2">
-      <BSDateDisplay />
-      </div>
-      <div className="flex justify-end">
-        <Image
-          src="/images/side-logo.png"
-          width={120}
-          height={103}
-          alt="PIMS"
-        />
+
+      <div className="flex flex-col items-center gap-2 pr-2">
+        <Image src="/images/flag.gif" width={40} height={40} alt="PIMS" />
+        <BSDateDisplay />
       </div>
     </div>
   )
