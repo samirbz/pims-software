d4b2470c9dc21344adde5414206614803388e90a
samir <febsec14@gmail.com>
2024-07-15 22:13:20 +0545

    regex fix for register

--- src/lib/schemas/registerSchema.ts
+++ src/lib/schemas/registerSchema.ts
@@ -4,9 +4,15 @@ export const registerSchema = z.object({
   name: z.string().min(3, {
     message: "name must be at least 3 characters",
   }),
-  username: z.string().min(3, {
-    message: "username must be at least 3 characters",
-  }),
+  username: z
+    .string()
+    .regex(/^[a-zA-Z0-9]+$/, {
+      message:
+        "Username must contain only letters (a-z, A-Z) and numbers (0-9)",
+    })
+    .min(3, {
+      message: "Username must be at least 3 characters",
+    }),
   email: z.string().min(3),
   password: z.string().min(5, {
     message: "password must be at least 5 characters",
