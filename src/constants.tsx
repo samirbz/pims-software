dcea1e24f2e42bedcbc1520cfa5a21362d242256
samir <febsec14@gmail.com>
2024-07-17 09:55:51 +0545

    icon changed in constants

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,41 +1,43 @@
-
-import { SideNavItem } from './types';
+import { CiFolderOn, CiHome, CiSettings } from "react-icons/ci"
+import { SideNavItem } from "./types"
+import { FaRegMessage } from "react-icons/fa6"
+import { IoIosHelpCircleOutline } from "react-icons/io"
 
 export const SIDENAV_ITEMS: SideNavItem[] = [
   {
-    title: 'Home',
-    path: '/',
-    icon: <Icon icon="lucide:home" width="24" height="24" />,
+    title: "Home",
+    path: "/",
+    icon: <CiHome width="24" height="24" />,
   },
   {
-    title: 'Projects',
-    path: '/projects',
-    icon: <Icon icon="lucide:folder" width="24" height="24" />,
+    title: "Projects",
+    path: "/projects",
+    icon: <CiFolderOn width="24" height="24" />,
     submenu: true,
     subMenuItems: [
-      { title: 'All', path: '/projects' },
-      { title: 'Web Design', path: '/projects/web-design' },
-      { title: 'Graphic Design', path: '/projects/graphic-design' },
+      { title: "All", path: "/projects" },
+      { title: "Web Design", path: "/projects/web-design" },
+      { title: "Graphic Design", path: "/projects/graphic-design" },
     ],
   },
   {
-    title: 'Messages',
-    path: '/messages',
-    icon: <Icon icon="lucide:mail" width="24" height="24" />,
+    title: "Messages",
+    path: "/messages",
+    icon: <FaRegMessage width="24" height="24" />,
   },
   {
-    title: 'Settings',
-    path: '/settings',
-    icon: <Icon icon="lucide:settings" width="24" height="24" />,
+    title: "Settings",
+    path: "/settings",
+    icon: <CiSettings width="24" height="24" />,
     submenu: true,
     subMenuItems: [
-      { title: 'Account', path: '/settings/account' },
-      { title: 'Privacy', path: '/settings/privacy' },
+      { title: "Account", path: "/settings/account" },
+      { title: "Privacy", path: "/settings/privacy" },
     ],
   },
   {
-    title: 'Help',
-    path: '/help',
-    icon: <Icon icon="lucide:help-circle" width="24" height="24" />,
+    title: "Help",
+    path: "/help",
+    icon: <IoIosHelpCircleOutline width="24" height="24" />,
   },
-];
+]
