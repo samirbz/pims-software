8b51f0f63bd983a6d679a26db53444ac37672caa
samir <febsec14@gmail.com>
2024-07-20 08:56:32 +0545

    snum from schema removed seed.ts created

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client"
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.user.createMany({
+    data: [
+      {
+        name: "sameer bajracharya",
+        username: "sam17",
+        email: "admin",
+        passwordHash:
+          "$2a$10$FNNpHEghXzmMucQpJZlXcOkYOPoyWgLVLqYzxWKaZq7Gcpk9ZdZn2",
+        createdby: "admin",
+      },
+    ],
+    skipDuplicates: true, // Skip inserting users if they already exist
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
