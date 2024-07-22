f6eb0a5a0657b0b6a8ee32c1bfc2d9c61621ba0b
samir <febsec14@gmail.com>
2024-07-22 12:16:20 +0545

    edit button created

--- src/app/members/office/fiscal-year/page.tsx
+++ src/app/members/office/fiscal-year/page.tsx
@@ -1,11 +1,18 @@
 "use client"
 import React, { useEffect, useState } from "react"
-import { Button } from "@nextui-org/react"
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from "@nextui-org/react"
 import { FaRegSave } from "react-icons/fa"
 import { FaCirclePlus } from "react-icons/fa6"
 import { NepaliDatePicker } from "nepali-datepicker-reactjs"
 import "nepali-datepicker-reactjs/dist/index.css"
 import { fetchFyData, saveFiscalYearDate } from "@/actions/formAction"
+import { MdModeEditOutline } from "react-icons/md"
 
 export default function FiscalYearPage() {
   const [startDate, setStartDate] = useState<string>("")
@@ -41,7 +48,7 @@ export default function FiscalYearPage() {
               <label htmlFor="date">आ.व सुरु मिति</label>
               <NepaliDatePicker
                 inputClassName="form-control"
-                className="border"
+                className="rounded-lg border p-1"
                 value={startDate}
                 onChange={(value: string) => setStartDate(value)}
                 options={{ calenderLocale: "ne", valueLocale: "en" }}
@@ -52,7 +59,7 @@ export default function FiscalYearPage() {
             <p>आ.व अन्तिम मिति</p>
             <NepaliDatePicker
               inputClassName="form-control"
-              className="border"
+              className="rounded-lg border p-1 "
               value={endDate}
               onChange={(value: string) => setEndDate(value)}
               options={{ calenderLocale: "ne", valueLocale: "en" }}
@@ -63,7 +70,7 @@ export default function FiscalYearPage() {
         <div className="flex items-center gap-4">
           <p>arthik barsha</p>
           <select
-            className="rounded-sm border p-1"
+            className="rounded-lg  border p-1 "
             value={fy}
             onChange={(event) => setFy(event.target.value)}
           >
@@ -96,6 +103,7 @@ export default function FiscalYearPage() {
                   आ.व अन्तिम मिति
                 </th>
                 <th className="border border-gray-200 px-4 py-2">आ.व</th>
+                <th className="border border-gray-200 px-4 py-2">Edit</th>
               </tr>
             </thead>
             <tbody>
@@ -112,6 +120,26 @@ export default function FiscalYearPage() {
                   </td>
                   <td className="border border-gray-200 px-4 py-2">
                     {year.fy}
+                  </td>
+                  <td className="border border-gray-200 px-4 py-2">
+                    <Dropdown>
+                      <DropdownTrigger>
+                        <Button variant="shadow" size="sm">
+                          <MdModeEditOutline />
+                        </Button>
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label="Static Actions">
+                        <DropdownItem>Edit</DropdownItem>
+
+                        <DropdownItem
+                          key="delete"
+                          className="text-danger"
+                          color="danger"
+                        >
+                          Delete
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
                   </td>
                 </tr>
               ))}
