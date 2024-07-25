2903d1852f0839046a0373d7e300619f1f64fe3e
samir <febsec14@gmail.com>
2024-07-25 13:05:50 +0545

    some form created of setup

--- src/app/members/setup/labTest/page.tsx
+++ src/app/members/setup/labTest/page.tsx
@@ -1,7 +1,76 @@
-import React from 'react'
+"use client"
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+} from "@nextui-org/react"
+import { FaRegSave } from "react-icons/fa"
+import "nepali-datepicker-reactjs/dist/index.css"
+import { MdModeEditOutline } from "react-icons/md"
 
-export default function LabTest() {
+export default function LagatSrot() {
   return (
-    <div>LabTest</div>
+    <div className="mt-8 flex flex-col items-center">
+      <div className="w-full px-4 sm:w-auto">
+        <h1 className="form-title">Lab Test Office</h1>
+        <br />
+        <div className="flex w-auto flex-col sm:gap-2">
+          <div className="flex flex-col gap-2">
+            <Input type="text" label="कार्यालय नाम" size="sm" />
+            <Input type="text" label=" ठेगान " size="sm" />
+
+            <Button
+              color="secondary"
+              className="w-10 self-center"
+              startContent={<FaRegSave />}
+            >
+              Save
+            </Button>
+          </div>
+          <div className="mb-2 max-h-[22rem] w-auto max-w-[90rem] overflow-auto sm:mb-0">
+            <table className="min-w-[30rem] border-collapse border ">
+              <thead className="sticky top-0  z-20 border-r-2 bg-purple-400">
+                <tr>
+                  <th className=" px-4 py-2">सि.न.</th>
+                  <th className=" px-4 py-2">कार्यालय नाम</th>
+                  <th className=" px-4 py-2">ठेगान</th>
+                  <th className=" px-4 py-2">Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="w-auto text-center">
+                  <td className="border px-4 py-2"></td>
+                  <td className="border px-4 py-2"></td>
+                  <td className="border px-4 py-2"></td>
+                  <td className="border px-4 py-2">
+                    <Dropdown>
+                      <DropdownTrigger>
+                        <Button className="z-10" variant="shadow" size="sm">
+                          <MdModeEditOutline />
+                        </Button>
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label="Static Actions">
+                        <DropdownItem>Edit</DropdownItem>
+
+                        <DropdownItem
+                          key="delete"
+                          className="text-danger"
+                          color="danger"
+                        >
+                          Delete
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/app/members/setup/yojanaKaryaBivaran/page.tsx
+++ src/app/members/setup/yojanaKaryaBivaran/page.tsx
@@ -1,7 +1,94 @@
-import React from 'react'
+"use client"
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Input,
+  Select,
+  SelectItem,
+} from "@nextui-org/react"
+import { FaRegSave } from "react-icons/fa"
+import "nepali-datepicker-reactjs/dist/index.css"
+import { MdModeEditOutline } from "react-icons/md"
+
+const selectConfig = [
+  { key: "1", label: "सशर्त अनुदान" },
+  { key: "2", label: "निशर्त अनुदान" },
+  { key: "3", label: "समपुरक अनुदान " },
+  { key: "4", label: "बिशेष अनुदान" },
+  { key: "5", label: "समानिकरण अनुदान" },
+  { key: "6", label: "आन्तरिक श्रोत" },
+  { key: "7", label: "संघिय सरकार " },
+  { key: "8", label: "प्रदेश सरकार " },
+  { key: "9", label: "प्रदेश सर्शत" },
+]
 
 export default function YojanaKaryaBivaran() {
   return (
-    <div>YojanaKaryaBivaran</div>
+    <div className="mt-8 flex flex-col items-center">
+      <div className="w-full px-4 sm:w-auto">
+        <h1 className="form-title">योजनाको किसिम अनुसार कार्य बिवरण </h1>
+        <br />
+        <div className="flex w-auto flex-col sm:gap-2">
+          <div className="flex flex-col gap-2">
+            <Select label="अनुदान को किसिम " className="max-w-xs">
+              {selectConfig.map((item) => (
+                <SelectItem key={item.key}>{item.label}</SelectItem>
+              ))}
+            </Select>
+            <Input type="text" label="लागत श्रोत नाम" size="sm" />
+
+            <Button
+              color="secondary"
+              className="w-10 self-center"
+              startContent={<FaRegSave />}
+            >
+              Save
+            </Button>
+          </div>
+          <div className="mb-2 max-h-[22rem] w-auto max-w-[90rem] overflow-auto sm:mb-0">
+            <table className="min-w-[30rem] border-collapse border ">
+              <thead className="sticky top-0  z-20 border-r-2 bg-purple-400">
+                <tr>
+                  <th className=" px-4 py-2">सि.न.</th>
+                  <th className=" px-4 py-2">योजनाको कार्य</th>
+                  <th className=" px-4 py-2">योजनाको किसिम</th>
+                  <th className=" px-4 py-2">Edit</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="w-auto text-center">
+                  <td className="border px-4 py-2"></td>
+                  <td className="border px-4 py-2"></td>
+                  <td className="border px-4 py-2"></td>
+                  <td className="border px-4 py-2">
+                    <Dropdown>
+                      <DropdownTrigger>
+                        <Button className="z-10" variant="shadow" size="sm">
+                          <MdModeEditOutline />
+                        </Button>
+                      </DropdownTrigger>
+                      <DropdownMenu aria-label="Static Actions">
+                        <DropdownItem>Edit</DropdownItem>
+
+                        <DropdownItem
+                          key="delete"
+                          className="text-danger"
+                          color="danger"
+                        >
+                          Delete
+                        </DropdownItem>
+                      </DropdownMenu>
+                    </Dropdown>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
