c4723b57e941cdda67e9c811755ae5b4a98d1585
samir <febsec14@gmail.com>
2024-08-29 23:20:50 +0545

    unused excel button removed

--- src/components/dashboard/menuItems/office/StaffDetails.tsx
+++ src/components/dashboard/menuItems/office/StaffDetails.tsx
@@ -35,7 +35,6 @@ import { staffRegister } from "@/actions/authActions"
 import { useForm } from "react-hook-form"
 import { IoIosSave, IoMdCheckmark } from "react-icons/io"
 import { StaffRegisterSchema } from "@/lib/schemas/staffRegisterSchema"
-import * as XLSX from "xlsx"
 import { MdModeEditOutline } from "react-icons/md"
 
 interface Member {
@@ -145,14 +144,6 @@ export default function StaffDetailPage() {
     }
     onOpenChange() // Close the modal
   }
-  // Function to export the table data to Excel
-  const exportToExcel = () => {
-    const worksheet = XLSX.utils.json_to_sheet(members)
-    const workbook = XLSX.utils.book_new()
-    XLSX.utils.book_append_sheet(workbook, worksheet, "Staff Details")
-    XLSX.writeFile(workbook, "StaffDetails.xlsx")
-  }
-
   const handleEdit = (member: Member) => {
     setEditMember(member)
     setValue("name", member.name) // Pre-fill the form fields
@@ -328,9 +319,7 @@ export default function StaffDetailPage() {
           >
             Add Staff
           </Button>
-          <Button color="primary" onClick={exportToExcel}>
-            Export to Excel
-          </Button>
+      
         </div>
       </div>
       <br />
