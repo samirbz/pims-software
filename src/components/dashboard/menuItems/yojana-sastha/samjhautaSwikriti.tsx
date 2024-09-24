c0186a11a80f9090f678fa57ad982c7c9d2fad11
samir <febsec14@gmail.com>
2024-09-24 14:14:31 +0545

    samjhauta swikriti saved

--- src/components/dashboard/menuItems/yojana-sastha/samjhautaSwikriti.tsx
+++ src/components/dashboard/menuItems/yojana-sastha/samjhautaSwikriti.tsx
@@ -15,28 +15,127 @@ import {
 import { FaRegSave } from "react-icons/fa"
 import { NepaliDatePicker } from "nepali-datepicker-reactjs"
 import "nepali-datepicker-reactjs/dist/index.css"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { MdOutlineSupervisorAccount } from "react-icons/md"
 import { CiSearch } from "react-icons/ci"
+import {
+  fetchYojanaDartaData,
+  saveYojanaSwikritiTippani,
+} from "@/actions/formAction"
+import { toast } from "react-toastify"
 
 export default function SamjhautaSwikriti() {
-  const [date, setDate] = useState<string>("")
+  const [aawa, setAawa] = useState("")
+  const [miti, setMiti] = useState("")
+  const [yojanaKaryaKramKoNaam, setYojanaKaryaKramKoNaam] = useState("")
+  const [upavoktaSamitiKoNaam, setUpavoktaSamitiKoNaam] = useState("")
+  const [adhyachyaKoNaam, setAdhyachyaKoNaam] = useState("")
+  const [velamaUpasthitiSankhya, setVelamaUpasthitiSankhya] = useState("")
+  const [padakariSankhya, setPadakariSankhya] = useState("")
+  const [mahilaSankhya, setMahilaSankhya] = useState("")
+  const [lagatAnumanRakam, setLagatAnumanRakam] = useState("")
+  const [nagarpalikaRakamRu, setNagarpalikaRakamRu] = useState("")
+  const [lagatSramDan, setlagatSramDan] = useState("")
+  const [contengencyRakam, setContengencyRakam] = useState("")
+  const [khudPauneRakam, setKhudPauneRakam] = useState("")
+  const [anugamanSamitikaSadasya, setAnugamanSamitikaSadasya] = useState("")
+  const [budgetKitabSNum, setVudgetKitabSNum] = useState("")
+  const [ushaGathanMiti, setUshaGathanMiti] = useState("")
+  const [mukhyaSamitiKoNaam, setMukhyaSamitiKoNaam] = useState("")
+  const [ushaNibedandiyiyekoMiti, setUshaNibedandiyiyekoMiti] = useState("")
+  const [anyaTipaniBivaran, setAnyaTipaniBivaran] = useState("")
+  const [yojanakoNaam, setYojanakoNaam] = useState("")
+  const [wadaNum, setWadaNum] = useState("")
+  const [biniyojitRakamRu, setBiniyojitRakamRu] = useState("")
+  const [sanyojak, setSanyojak] = useState("")
+  const [sadasyaOne, setSadasyaOne] = useState("")
+  const [sadasyaTwo, setSadasyaTwo] = useState("")
 
-  const animals = [
-    { key: "cat", label: "1234567890123456789" },
-    { key: "dog", label: "Dog" },
-    { key: "elephant", label: "Elephant" },
-    { key: "lion", label: "Lion" },
-    { key: "tiger", label: "Tiger" },
-    { key: "giraffe", label: "Giraffe" },
-    { key: "dolphin", label: "Dolphin" },
-    { key: "penguin", label: "Penguin" },
-    { key: "zebra", label: "Zebra" },
-    { key: "shark", label: "Shark" },
-    { key: "whale", label: "Whale" },
-    { key: "otter", label: "Otter" },
-    { key: "crocodile", label: "Crocodile" },
-  ]
+  const [yojanaKoNaam, setYojanaKoNaam] = useState<any[]>([])
+
+  // const [btnDisable, setBtnDisable] = useState(false)
+
+  const fetchYojanaDartaKoNaamData = async () => {
+    try {
+      const data = await fetchYojanaDartaData()
+      console.log("Fetched Anudaan Data:", data) // For debugging
+      setYojanaKoNaam(data)
+    } catch (e) {
+      console.error("Error fetching anudaan data", e)
+    }
+  }
+
+  const onSubmit = async () => {
+    const result = await saveYojanaSwikritiTippani(
+      aawa,
+      miti,
+      yojanaKaryaKramKoNaam,
+      upavoktaSamitiKoNaam,
+      adhyachyaKoNaam,
+      velamaUpasthitiSankhya,
+      padakariSankhya,
+      mahilaSankhya,
+      lagatAnumanRakam,
+      nagarpalikaRakamRu,
+      lagatSramDan,
+      contengencyRakam,
+      khudPauneRakam,
+      anugamanSamitikaSadasya,
+      budgetKitabSNum,
+      ushaGathanMiti,
+      mukhyaSamitiKoNaam,
+      ushaNibedandiyiyekoMiti,
+      anyaTipaniBivaran,
+      yojanakoNaam,
+      wadaNum,
+      biniyojitRakamRu,
+      sanyojak,
+      sadasyaOne,
+      sadasyaTwo
+    )
+    if (result.status === "success") {
+      setAawa("")
+      setMiti("")
+      setYojanaKaryaKramKoNaam("")
+      setUpavoktaSamitiKoNaam("")
+      setAdhyachyaKoNaam("")
+      setVelamaUpasthitiSankhya("")
+      setPadakariSankhya("")
+      setMahilaSankhya("")
+      setLagatAnumanRakam("")
+      setNagarpalikaRakamRu("")
+      setlagatSramDan("")
+      setContengencyRakam("")
+      setKhudPauneRakam("")
+      setAnugamanSamitikaSadasya("")
+      setVudgetKitabSNum("")
+      setUshaGathanMiti("")
+      setMukhyaSamitiKoNaam("")
+      setUshaNibedandiyiyekoMiti("")
+      setAnyaTipaniBivaran("")
+      setYojanakoNaam("")
+      setWadaNum("")
+      setBiniyojitRakamRu("")
+      setSanyojak("")
+      setSadasyaOne("")
+      setSadasyaTwo("")
+      toast.success("successfully created")
+    } else {
+      console.error("Error occurred during save")
+    }
+  }
+
+  useEffect(() => {
+    fetchYojanaDartaKoNaamData()
+  }, [])
+
+  useEffect(() => {
+    setLagatAnumanRakam("")
+    setNagarpalikaRakamRu("")
+    setlagatSramDan("")
+    setContengencyRakam("")
+    setKhudPauneRakam("")
+  }, [])
 
   const { isOpen, onOpen, onOpenChange } = useDisclosure()
 
@@ -50,43 +149,80 @@ export default function SamjhautaSwikriti() {
         <div className="flex w-auto flex-col sm:gap-2">
           <div className="flex flex-col gap-2">
             <div className="flex gap-2">
-              <Input type="text" label=" आ.व " size="sm" className="w-1/2" />
+              <Input
+                type="text"
+                label=" आ.व "
+                size="sm"
+                className="w-1/2"
+                value={aawa}
+                onChange={(e) => setAawa(e.target.value)}
+              />
               <form className="flex items-center gap-2 ">
                 <label htmlFor="date">मितिः-</label>
                 <NepaliDatePicker
                   inputClassName="form-control"
                   className="rounded-lg border p-1 "
-                  value={date}
-                  onChange={(value: string) => setDate(value)}
+                  value={miti}
+                  onChange={(value: string) => setMiti(value)}
                   options={{ calenderLocale: "ne", valueLocale: "en" }}
                 />
               </form>
             </div>
-            <Select label="योजना / कार्यक्रमको नाम" size="sm" fullWidth>
-              {animals.map((animal) => (
-                <SelectItem key={animal.key}>{animal.label}</SelectItem>
+            <Select
+              label="योजना / कार्यक्रमको नाम"
+              size="sm"
+              placeholder="Select an option" // Optional: if you want a placeholder
+              selectedKeys={
+                yojanaKaryaKramKoNaam
+                  ? new Set([yojanaKaryaKramKoNaam])
+                  : new Set()
+              } // Binding the selected value
+              onSelectionChange={(keys) => {
+                const selectedValue = Array.from(keys).join(", ")
+                setYojanaKaryaKramKoNaam(selectedValue)
+              }}
+            >
+              {yojanaKoNaam.map((item) => (
+                <SelectItem key={item.yojanaKoNaam} value={item.yojanaKoNaam}>
+                  {item.yojanaKoNaam}
+                </SelectItem>
               ))}
             </Select>
-            <Input type="text" label="उपभोक्ता समितिको नाम " size="sm" />
-            <Input type="text" label="अध्यक्षको नाम" size="sm" />
+            <Input
+              type="text"
+              label="उपभोक्ता समितिको नाम "
+              size="sm"
+              value={upavoktaSamitiKoNaam}
+              onChange={(e) => setUpavoktaSamitiKoNaam(e.target.value)}
+            />
+            <Input
+              type="text"
+              label="अध्यक्षको नाम"
+              size="sm"
+              value={adhyachyaKoNaam}
+              onChange={(e) => setAdhyachyaKoNaam(e.target.value)}
+            />
             <div className="flex gap-2">
               <Input
                 type="Number"
                 label="भेलामा उपस्थिती संख्या"
                 size="sm"
-                className="w-[66.66%]"
+                value={velamaUpasthitiSankhya}
+                onChange={(e) => setVelamaUpasthitiSankhya(e.target.value)}
               />
               <Input
                 type="Number"
                 label="पदाधिकारी संख्या"
                 size="sm"
-                className="w-[66.66%]"
+                value={padakariSankhya}
+                onChange={(e) => setPadakariSankhya(e.target.value)}
               />
               <Input
                 type="Number"
                 label="महिला संख्या"
                 size="sm"
-                className="w-[66.66%]"
+                value={mahilaSankhya}
+                onChange={(e) => setMahilaSankhya(e.target.value)}
               />
             </div>
             <div className="flex gap-2">
@@ -96,7 +232,7 @@ export default function SamjhautaSwikriti() {
                 label=" लागत अनुमान रकम "
                 size="sm"
                 color="success"
-                value="12345"
+                value={lagatAnumanRakam}
               />
               <Input
                 isReadOnly
@@ -104,15 +240,15 @@ export default function SamjhautaSwikriti() {
                 label=" नगरपालिका रकम रु. "
                 color="success"
                 size="sm"
-                value="12345"
+                value={nagarpalikaRakamRu}
               />
               <Input
                 isReadOnly
                 type="Number"
                 label=" लागत श्रमदान "
-                value="12345"
                 size="sm"
                 color="success"
+                value={lagatSramDan}
               />
             </div>
             <div className="flex gap-2">
@@ -122,7 +258,7 @@ export default function SamjhautaSwikriti() {
                 label="कन्टेन्जेन्सी रकम"
                 color="success"
                 size="sm"
-                value="12345"
+                value={contengencyRakam}
               />
               <Input
                 isReadOnly
@@ -130,9 +266,15 @@ export default function SamjhautaSwikriti() {
                 label=" खुद पाउने रकम"
                 color="success"
                 size="sm"
-                value="12345"
+                value={khudPauneRakam}
               />
-              <Input type="Number" label="अनुगमन समितिका सदस्य" size="sm" />
+              <Input
+                type="Number"
+                label="अनुगमन समितिका सदस्य"
+                size="sm"
+                value={anugamanSamitikaSadasya}
+                onChange={(e) => setAnugamanSamitikaSadasya(e.target.value)}
+              />
             </div>
 
             <div className="flex gap-2">
@@ -141,14 +283,16 @@ export default function SamjhautaSwikriti() {
                 label="बजेट किताबको सि.न."
                 size="sm"
                 className="w-1/2"
+                value={budgetKitabSNum}
+                onChange={(e) => setVudgetKitabSNum(e.target.value)}
               />
               <form className="flex items-center gap-2 ">
                 <label htmlFor="date">उ.स. गठन मिति:-</label>
                 <NepaliDatePicker
                   inputClassName="form-control"
                   className="rounded-lg border p-1 "
-                  value={date}
-                  onChange={(value: string) => setDate(value)}
+                  value={ushaGathanMiti}
+                  onChange={(value: string) => setUshaGathanMiti(value)}
                   options={{ calenderLocale: "ne", valueLocale: "en" }}
                 />
               </form>
@@ -160,6 +304,8 @@ export default function SamjhautaSwikriti() {
                 label="मुख्य समितिको नाम"
                 className="w-1/2"
                 size="sm"
+                value={mukhyaSamitiKoNaam}
+                onChange={(e) => setMukhyaSamitiKoNaam(e.target.value)}
               />
 
               <form className="flex items-center gap-2 ">
@@ -169,8 +315,10 @@ export default function SamjhautaSwikriti() {
                 <NepaliDatePicker
                   inputClassName="form-control"
                   className="rounded-lg border p-1 "
-                  value={date}
-                  onChange={(value: string) => setDate(value)}
+                  value={ushaNibedandiyiyekoMiti}
+                  onChange={(value: string) =>
+                    setUshaNibedandiyiyekoMiti(value)
+                  }
                   options={{ calenderLocale: "ne", valueLocale: "en" }}
                 />
               </form>
@@ -188,13 +336,19 @@ export default function SamjhautaSwikriti() {
               label="अन्य टिप्पणी विवरण"
               className="w-full"
               variant="bordered"
+              value={anyaTipaniBivaran}
+              onChange={(e) => setAnyaTipaniBivaran(e.target.value)}
             />
             <div className="flex justify-between">
               <Button color="default" className="flex justify-start">
                 Reset Form
               </Button>
               <div className="flex justify-end gap-2">
-                <Button color="secondary" startContent={<FaRegSave />}>
+                <Button
+                  color="secondary"
+                  startContent={<FaRegSave />}
+                  onClick={onSubmit}
+                >
                   Save
                 </Button>
                 <Button color="default" startContent={<FaRegSave />}>
@@ -214,9 +368,24 @@ export default function SamjhautaSwikriti() {
               </ModalHeader>
               <ModalBody>
                 <div className="flex gap-2">
-                  <Select label="योजनाको नाम" size="sm" fullWidth>
-                    {animals.map((animal) => (
-                      <SelectItem key={animal.key}>{animal.label}</SelectItem>
+                  <Select
+                    label="योजनाको नाम"
+                    size="sm"
+                    selectedKeys={
+                      yojanakoNaam ? new Set([yojanakoNaam]) : new Set()
+                    } // Binding the selected value
+                    onSelectionChange={(keys) => {
+                      const selectedValue = Array.from(keys).join(", ")
+                      setYojanakoNaam(selectedValue)
+                    }}
+                  >
+                    {yojanaKoNaam.map((item) => (
+                      <SelectItem
+                        key={item.yojanaKoNaam}
+                        value={item.yojanaKoNaam}
+                      >
+                        {item.yojanaKoNaam}
+                      </SelectItem>
                     ))}
                   </Select>
                   <Input
@@ -224,12 +393,16 @@ export default function SamjhautaSwikriti() {
                     label="वडा न."
                     className="w-1/2"
                     size="sm"
+                    value={wadaNum}
+                    onChange={(e) => setWadaNum(e.target.value)}
                   />
                   <Input
                     type="number"
                     label="विनियोजित रकम रु."
                     className="w-1/2"
                     size="sm"
+                    value={biniyojitRakamRu}
+                    onChange={(e) => setBiniyojitRakamRu(e.target.value)}
                   />
                 </div>
 
@@ -241,18 +414,24 @@ export default function SamjhautaSwikriti() {
                     label="संयोजक"
                     className="w-1/2"
                     size="sm"
+                    value={sanyojak}
+                    onChange={(e) => setSanyojak(e.target.value)}
                   />
                   <Input
                     type="text"
                     label="सदस्य- १"
                     className="w-1/2"
                     size="sm"
+                    value={sadasyaOne}
+                    onChange={(e) => setSadasyaOne(e.target.value)}
                   />
                   <Input
                     type="text"
                     label="सदस्य- २"
                     className="w-1/2"
                     size="sm"
+                    value={sadasyaTwo}
+                    onChange={(e) => setSadasyaTwo(e.target.value)}
                   />
                 </div>
               </ModalBody>
