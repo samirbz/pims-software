df9a23f2182f073b998e84b0a329659198de53f9
samir <febsec14@gmail.com>
2024-10-21 22:12:50 +0545

    data given for print

--- src/components/dashboard/menuItems/yojana-sastha/samjhautaSwikriti.tsx
+++ src/components/dashboard/menuItems/yojana-sastha/samjhautaSwikriti.tsx
@@ -55,6 +55,41 @@ export default function SamjhautaSwikriti() {
 
   const [yojanaKoNaam, setYojanaKoNaam] = useState<any[]>([])
 
+  // For print
+  const [anudanKoNaam, setanudanKoNaam] = useState("")
+  const [anudanKoNaam2, setanudanKoNaam2] = useState("")
+  const [anudanKoNaam3, setanudanKoNaam3] = useState("")
+  const [barsikYojana, setbarsikYojana] = useState(false)
+  const [biniyojitRakam, setbiniyojitRakam] = useState("")
+  const [budgetKitabSnum, setbudgetKitabSnum] = useState("")
+  const [budgetType, setbudgetType] = useState("")
+  const [contengencyResult, setcontengencyResult] = useState("")
+  const [dharautiRakam, setdharautiRakam] = useState("")
+  const [gharPariwarSankhya, setgharPariwarSankhya] = useState("")
+  const [janaSankhya, setjanaSankhya] = useState("")
+  const [karyaBivaran, setkaryaBivaran] = useState("")
+  const [karyagatSamuha, setkaryagatSamuha] = useState("")
+  const [kramagatYojana, setkramagatYojana] = useState(false)
+  const [lagatSrotAmount, setlagatSrotAmount] = useState("")
+  const [lagatSrotAmount2, setlagatSrotAmount2] = useState("")
+  const [lagatSrotAmount3, setlagatSrotAmount3] = useState("")
+  const [lagatSrotHaru, setlagatSrotHaru] = useState("")
+  const [lagatSrotHaru2, setlagatSrotHaru2] = useState("")
+  const [lagatSrotHaru3, setlagatSrotHaru3] = useState("")
+  const [markmatRakamResult, setmarkmatRakamResult] = useState("")
+  const [marmatRakam, setmarmatRakam] = useState("")
+  const [mukhyaSamiti, setmukhyaSamiti] = useState("")
+  const [prastabSwikritMiti, setprastabSwikritMiti] = useState("")
+  const [sabhaNirnayaMiti, setsabhaNirnayaMiti] = useState("")
+  const [thegana, setthegana] = useState("")
+  const [upalabdhiLakshya, setupalabdhiLakshya] = useState("")
+  const [uplabdhiLakhshyaQty, setuplabdhiLakhshyaQty] = useState("")
+  const [wada, setwada] = useState("")
+  const [yojanaKoKisim, setyojanaKoKisim] = useState("")
+  const [yojanaKoWada, setyojanaKoWada] = useState("")
+  const [yojanaSwikrit, setyojanaSwikrit] = useState("")
+  const [yojanaUpachetra, setyojanaUpachetra] = useState("")
+
   const [loading, setLoading] = useState(true)
 
   // const [btnDisable, setBtnDisable] = useState(false)
@@ -67,6 +102,69 @@ export default function SamjhautaSwikriti() {
     } catch (e) {
       console.error("Error fetching anudaan data", e)
     }
+  }
+
+  const showAllData = () => {
+    alert(`
+                      aawa = ${aawa}
+                      miti = ${miti}
+                      yojanaKaryaKramKoNaam = ${yojanaKaryaKramKoNaam}
+                      upavoktaSamitiKoNaam = ${upavoktaSamitiKoNaam}
+                      adhyachyaKoNaam = ${adhyachyaKoNaam}
+                      velamaUpasthitiSankhya = ${velamaUpasthitiSankhya}
+                      padakariSankhya = ${padakariSankhya}
+                      mahilaSankhya = ${mahilaSankhya}
+                      lagatAnumanRakam = ${lagatAnumanRakam}
+                      nagarpalikaRakamRu = ${nagarpalikaRakamRu}
+                      lagatSramDan = ${lagatSramDan}
+                      contengencyRakam = ${contengencyRakam}
+                      khudPauneRakam = ${khudPauneRakam}
+                      anugamanSamitikaSadasya = ${anugamanSamitikaSadasya}
+                      budgetKitabSNum = ${budgetKitabSNum}
+                      ushaGathanMiti = ${ushaGathanMiti}
+                      mukhyaSamitiKoNaam = ${mukhyaSamitiKoNaam}
+                      ushaNibedandiyiyekoMiti = ${ushaNibedandiyiyekoMiti}
+                      anyaTipaniBivaran = ${anyaTipaniBivaran}
+                      yojanakoNaam = ${yojanakoNaam}
+                      wadaNum = ${wadaNum}
+                      biniyojitRakamRu = ${biniyojitRakamRu}
+                      sanyojak = ${sanyojak}
+                      sadasyaOne = ${sadasyaOne}
+                      sadasyaTwo = ${sadasyaTwo}
+                      anudanKoNaam = ${anudanKoNaam}
+                      anudanKoNaam2 = ${anudanKoNaam2}
+                      anudanKoNaam3 = ${anudanKoNaam3}
+                      barsikYojana = ${barsikYojana}
+                      biniyojitRakam = ${biniyojitRakam}
+                      budgetKitabSnum = ${budgetKitabSnum}
+                      budgetType = ${budgetType}
+                      contengencyResult = ${contengencyResult}
+                      dharautiRakam = ${dharautiRakam}
+                      gharPariwarSankhya = ${gharPariwarSankhya}
+                      janaSankhya = ${janaSankhya}
+                      karyaBivaran = ${karyaBivaran}
+                      karyagatSamuha = ${karyagatSamuha}
+                      kramagatYojana = ${kramagatYojana}
+                      lagatSrotAmount = ${lagatSrotAmount}
+                      lagatSrotAmount2 = ${lagatSrotAmount2}
+                      lagatSrotAmount3 = ${lagatSrotAmount3}
+                      lagatSrotHaru = ${lagatSrotHaru}
+                      lagatSrotHaru2 = ${lagatSrotHaru2}
+                      lagatSrotHaru3 = ${lagatSrotHaru3}
+                      markmatRakamResult = ${markmatRakamResult}
+                      marmatRakam = ${marmatRakam}
+                      mukhyaSamiti = ${mukhyaSamiti}
+                      prastabSwikritMiti = ${prastabSwikritMiti}
+                      sabhaNirnayaMiti = ${sabhaNirnayaMiti}
+                      thegana = ${thegana}
+                      upalabdhiLakshya = ${upalabdhiLakshya}
+                      uplabdhiLakhshyaQty = ${uplabdhiLakhshyaQty}
+                      wada = ${wada}
+                      yojanaKoKisim = ${yojanaKoKisim}
+                      yojanaKoWada = ${yojanaKoWada}
+                      yojanaSwikrit = ${yojanaSwikrit}
+                      yojanaUpachetr = ${yojanaUpachetra}
+      `)
   }
 
   const onSubmit = async () => {
@@ -134,7 +232,6 @@ export default function SamjhautaSwikriti() {
       try {
         setLoading(true)
         const data = await getYojanaDartaForSwikriti(yojanaKaryaKramKoNaam)
-
         console.log(data)
 
         if (data && data.length > 0) {
@@ -142,6 +239,41 @@ export default function SamjhautaSwikriti() {
           const kulAnudaan = data[0].kulAnudaanRakam
           const janaSramdan = data[0].janaSramdanRakam
           const contengency = data[0].dharautiRakamResult
+
+          // Added for print
+          const anudanKoNaam = data[0].anudanKoNaam
+          const anudanKoNaam2 = data[0].anudanKoNaam2
+          const anudanKoNaam3 = data[0].anudanKoNaam3
+          const barsikYojana = data[0].barsikYojana
+          const biniyojitRakam = data[0].biniyojitRakam
+          const budgetKitabSnum = data[0].budgetKitabSnum
+          const budgetType = data[0].budgetType
+          const contengencyResult = data[0].contengencyResult
+          const dharautiRakam = data[0].dharautiRakam
+          const gharPariwarSankhya = data[0].gharPariwarSankhya
+          const janaSankhya = data[0].janaSankhya
+          const karyaBivaran = data[0].karyaBivaran
+          const karyagatSamuha = data[0].karyagatSamuha
+          const kramagatYojana = data[0].kramagatYojana
+          const lagatSrotAmount = data[0].lagatSrotAmount
+          const lagatSrotAmount2 = data[0].lagatSrotAmount2
+          const lagatSrotAmount3 = data[0].lagatSrotAmount3
+          const lagatSrotHaru = data[0].lagatSrotHaru
+          const lagatSrotHaru2 = data[0].lagatSrotHaru2
+          const lagatSrotHaru3 = data[0].lagatSrotHaru3
+          const markmatRakamResult = data[0].markmatRakamResult
+          const marmatRakam = data[0].marmatRakam
+          const mukhyaSamiti = data[0].mukhyaSamiti
+          const prastabSwikritMiti = data[0].prastabSwikritMiti
+          const sabhaNirnayaMiti = data[0].sabhaNirnayaMiti
+          const thegana = data[0].thegana
+          const upalabdhiLakshya = data[0].upalabdhiLakshya
+          const uplabdhiLakhshyaQty = data[0].uplabdhiLakhshyaQty
+          const wada = data[0].wada
+          const yojanaKoKisim = data[0].yojanaKoKisim
+          const yojanaKoWada = data[0].yojanaKoWada
+          const yojanaSwikrit = data[0].yojanaSwikrit
+          const yojanaUpachetra = data[0].yojanaUpachetra
 
           setLagatAnumanRakam(estimateAmount)
           setNagarpalikaRakamRu(kulAnudaan)
@@ -153,6 +285,41 @@ export default function SamjhautaSwikriti() {
               ? yojanaKaryaKramKoNaam + " (उपभोक्ता समिति)"
               : ""
           )
+
+          // Added fro print
+          setanudanKoNaam(anudanKoNaam)
+          setanudanKoNaam2(anudanKoNaam2)
+          setanudanKoNaam3(anudanKoNaam3)
+          setbarsikYojana(barsikYojana)
+          setbiniyojitRakam(biniyojitRakam)
+          setbudgetKitabSnum(budgetKitabSnum)
+          setbudgetType(budgetType)
+          setcontengencyResult(contengencyResult)
+          setdharautiRakam(dharautiRakam)
+          setgharPariwarSankhya(gharPariwarSankhya)
+          setjanaSankhya(janaSankhya)
+          setkaryaBivaran(karyaBivaran)
+          setkaryagatSamuha(karyagatSamuha)
+          setkramagatYojana(kramagatYojana)
+          setlagatSrotAmount(lagatSrotAmount)
+          setlagatSrotAmount2(lagatSrotAmount2)
+          setlagatSrotAmount3(lagatSrotAmount3)
+          setlagatSrotHaru(lagatSrotHaru)
+          setlagatSrotHaru2(lagatSrotHaru2)
+          setlagatSrotHaru3(lagatSrotHaru3)
+          setmarkmatRakamResult(markmatRakamResult)
+          setmarmatRakam(marmatRakam)
+          setmukhyaSamiti(mukhyaSamiti)
+          setprastabSwikritMiti(prastabSwikritMiti)
+          setsabhaNirnayaMiti(sabhaNirnayaMiti)
+          setthegana(thegana)
+          setupalabdhiLakshya(upalabdhiLakshya)
+          setuplabdhiLakhshyaQty(uplabdhiLakhshyaQty)
+          setwada(wada)
+          setyojanaKoKisim(yojanaKoKisim)
+          setyojanaKoWada(yojanaKoWada)
+          setyojanaSwikrit(yojanaSwikrit)
+          setyojanaUpachetra(yojanaUpachetra)
         }
       } catch (error) {
         console.error("Error fetching Yojana Darta data:", error)
@@ -386,9 +553,73 @@ export default function SamjhautaSwikriti() {
                 <Button
                   color="default"
                   startContent={<FaRegSave />}
-                  onClick={() => SamjhautaSwikritiPrint("yojana ko naam")}
+                  onClick={() =>
+                    SamjhautaSwikritiPrint(
+                      aawa,
+                      miti,
+                      yojanaKaryaKramKoNaam,
+                      upavoktaSamitiKoNaam,
+                      adhyachyaKoNaam,
+                      velamaUpasthitiSankhya,
+                      padakariSankhya,
+                      mahilaSankhya,
+                      lagatAnumanRakam,
+                      nagarpalikaRakamRu,
+                      lagatSramDan,
+                      contengencyRakam,
+                      khudPauneRakam,
+                      anugamanSamitikaSadasya,
+                      budgetKitabSNum,
+                      ushaGathanMiti,
+                      mukhyaSamitiKoNaam,
+                      ushaNibedandiyiyekoMiti,
+                      anyaTipaniBivaran,
+                      yojanakoNaam,
+                      wadaNum,
+                      biniyojitRakamRu,
+                      sanyojak,
+                      sadasyaOne,
+                      sadasyaTwo,
+                      anudanKoNaam,
+                      anudanKoNaam2,
+                      anudanKoNaam3,
+                      barsikYojana,
+                      biniyojitRakam,
+                      budgetKitabSnum,
+                      budgetType,
+                      contengencyResult,
+                      dharautiRakam,
+                      gharPariwarSankhya,
+                      janaSankhya,
+                      karyaBivaran,
+                      karyagatSamuha,
+                      kramagatYojana,
+                      lagatSrotAmount,
+                      lagatSrotAmount2,
+                      lagatSrotAmount3,
+                      lagatSrotHaru,
+                      lagatSrotHaru2,
+                      lagatSrotHaru3,
+                      markmatRakamResult,
+                      marmatRakam,
+                      mukhyaSamiti,
+                      prastabSwikritMiti,
+                      sabhaNirnayaMiti,
+                      thegana,
+                      upalabdhiLakshya,
+                      uplabdhiLakhshyaQty,
+                      wada,
+                      yojanaKoKisim,
+                      yojanaKoWada,
+                      yojanaSwikrit,
+                      yojanaUpachetra
+                    )
+                  }
                 >
                   Print
+                </Button>
+                <Button color="primary" onClick={showAllData}>
+                  View Detail
                 </Button>
               </div>
             </div>
