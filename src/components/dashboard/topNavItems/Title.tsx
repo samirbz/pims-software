f39cee966f4b3ae56728521918610973a38bcf82
samir <febsec14@gmail.com>
2025-01-12 19:57:00 +0545

    navbar nepali number changed

--- src/components/dashboard/topNavItems/Title.tsx
+++ src/components/dashboard/topNavItems/Title.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useMyContext, usePlaceContext } from "../../../context/MyContext"
+import { ConvertToNepaliNumerals } from "@/lib/util"
 
 export default function Title() {
   const { value } = useMyContext()
@@ -8,7 +9,7 @@ export default function Title() {
   return (
     <div className="mr-14 flex items-center gap-2">
       <p className="text-lg font-semibold text-gray-500">
-        {place} - {value === null ? "waiting..." : value}
+        {place} - {value === null ? "waiting..." : ConvertToNepaliNumerals(value)}
       </p>
     </div>
   )
